43637b47dc86031f8cede2dc2775c15938b8cbb2
AnliYang <anli.face@gmail.com>
2015-11-05 14:12:43 -0800

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+# note: in this case array index number is equal to position number (rather than indez = position - 1)
+
+def position_taken?(board, position)
+  if board[position] == "X" || board[position] == "O"
+    true
+  else
+    false
+  end
+end
